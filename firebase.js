8891ede58b6b316cbd975b59a43a6ef2ac2334ed
ishayagerrand8-crypto <ishayagerrand8@gmail.com>
2026-06-13 02:22:38 +0100

    Refactor firebase.js to keep configuration only

--- firebase.js
+++ firebase.js
@@ -1,12 +1,3 @@
-// Firebase SDK Imports
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-
-// Your Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyBqfgZ5viIKnzhXMVYctbf-L3NTaSo6bSI",
   authDomain: "let-s-chat-3f5c2.firebaseapp.com",
@@ -15,12 +6,3 @@ const firebaseConfig = {
   messagingSenderId: "870798798773",
   appId: "1:870798798773:web:4bf5120353b75fcff073fc"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-// Make available globally
-window.auth = auth;
-window.createUserWithEmailAndPassword = createUserWithEmailAndPassword;
-window.signInWithEmailAndPassword = signInWithEmailAndPassword;
