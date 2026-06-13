551e1a3b2c83a307b2c8d1892c637201c6e1a8b4
ishayagerrand8-crypto <ishayagerrand8@gmail.com>
2026-06-13 03:37:23 +0100

    Delete firebase.js

--- firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBqfgZ5viIKnzhXMVYctbf-L3NTaSo6bSI",
-  authDomain: "let-s-chat-3f5c2.firebaseapp.com",
-  projectId: "let-s-chat-3f5c2",
-  storageBucket: "let-s-chat-3f5c2.firebasestorage.app",
-  messagingSenderId: "870798798773",
-  appId: "1:870798798773:web:4bf5120353b75fcff073fc"
-};
