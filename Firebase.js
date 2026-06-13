47a641c3f4f165e0a6bfd7f6f602e7e76695c024
ishayagerrand8-crypto <ishayagerrand8@gmail.com>
2026-06-13 11:32:05 +0100

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -1,1 +1,17 @@
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBqfgZ5viIKnzhXMVYctbf-L3NTaSo6bSI",
+  authDomain: "let-s-chat-3f5c2.firebaseapp.com",
+  projectId: "let-s-chat-3f5c2",
+  storageBucket: "let-s-chat-3f5c2.firebasestorage.app",
+  messagingSenderId: "870798798773",
+  appId: "1:870798798773:web:4bf5120353b75fcff073fc"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+console.log("Firebase Connected!");
