4ad335f1972f44c3ef23fd085b69f0fd1967d43d
ishayagerrand8-crypto <ishayagerrand8@gmail.com>
2026-06-13 03:49:52 +0100

    Initialize Firebase with configuration settings

--- firebase.js
+++ firebase.js
@@ -1,3 +1,9 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBqfgZ5viIKnzhXMVYctbf-L3NTaSo6bSI",
   authDomain: "let-s-chat-3f5c2.firebaseapp.com",
@@ -6,3 +12,6 @@ const firebaseConfig = {
   messagingSenderId: "870798798773",
   appId: "1:870798798773:web:4bf5120353b75fcff073fc"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
